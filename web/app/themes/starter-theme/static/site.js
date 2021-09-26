2fddb21d000faac895d2634c80ea68877ae84539
ricardocotillo <ricardo.cotillo@gmail.com>
2021-09-26 16:56:30 -0500

    added showcase animation

--- web/app/themes/starter-theme/static/site.js
+++ web/app/themes/starter-theme/static/site.js
@@ -2,22 +2,42 @@ jQuery( document ).ready( function( $ ) {
   // Parallax
   const rellax = new Rellax('.rellax')
 
-  // Menu
+  // variables
   const ham = $('.hamburger')
   const showcase = $('#showcase')
   const zoom = $('.product-zoom')
   const nav = $('#nav-main')
+  let currentShowcase;
+
+  // Showcase
   zoom.on('click', function(e) {
+    const img = $(this).parent().find('.attachment-woocommerce_thumbnail')
+    const verticalCenterPoint = $(window).height() / 2
+    const horizontalCenterPoint = $(window).width() / 2
+    const offset = img.offset()
+    const width = img.width()
+    const height = img.height()
+    const x = horizontalCenterPoint - offset.left - (width / 2)
+    const y = verticalCenterPoint - offset.top - (height / 2)
+    img.css('transform', `translate(${x}px, ${y}px)`)
+    img.css('opacity', 0)
     const url = $(this).data('full')
     showcase.find('.showcase-img').attr('src', url)
-    showcase.css('display', 'flex')
+    showcase.css('height', '100vh')
+    showcase.css('opacity', 1)
+    currentShowcase = img
   })
 
   showcase.find('.close').on('click', function(e) {
-    showcase.css('display', 'none')
+    showcase.css('opacity', 0)
+    showcase.css('height', 0)
     showcase.find('.showcase-img').attr('src', '')
+
+    currentShowcase.css('transform', '')
+    currentShowcase.css('opacity', 1)
   })
 
+  // Menu
   ham.on('click', function(e) {
     $(this).siblings().show()
     $(this).hide()
