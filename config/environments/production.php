51f8b2f9ca92d2e8584b326f53c64215f3cffe13
ricardocotillo <ricardo.cotillo@gmail.com>
2021-11-06 09:31:36 -0500

    added production config

---
+++ config/environments/production.php
@@ -0,0 +1,7 @@
+<?php
+use Roots\WPConfig\Config;
+
+Config::define('WP_CACHE', true);
+Config::define('WP_DEBUG', true);
+Config::define('WP_DEBUG_DISPLAY', false);
+Config::define('WP_DEBUG_LOG', true);
